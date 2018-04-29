aa7066bff9492025c2eaa795b56d9ef520cee83a
sine loco <ladimir@gmail.com>
2018-04-30 02:26:36 +0300

    gradle now runs test :)

--- src/test/java/ru/snm/misc/testng_with_groovy/ClassToTestJavaTest.java
+++ src/test/java/ru/snm/misc/testng_with_groovy/ClassToTestJavaTest.java
@@ -1,20 +1,26 @@
 package ru.snm.misc.testng_with_groovy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
 
 /**
  * @author sine-loco
  */
 public class ClassToTestJavaTest {
+    private final static Logger logger = LogManager.getLogger();
 
     private ClassToTest ctt;
 
     @BeforeClass
     void beforeClass() {
+        logger.info( "before class" );
+
         ctt = new ClassToTest();
     }
 
@@ -26,12 +32,13 @@ public class ClassToTestJavaTest {
     @Test( dependsOnMethods = "doSmthnWithString_throws",
             dataProvider = "forDoSmthn" )
     public void doSmthnWithString( String input, String expected ) {
+        logger.info( "test doSmthnWithString - input: [{}]", input );
+
         // when
         String actual = ctt.doSmthnWithString( input );
 
         // expect
         assertEquals( actual, expected );
-
     }
 
     @DataProvider
